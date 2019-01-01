41aa0c8b9a8a19017e4bebd7db3a4f2fc1025649
Ana Harris <vana1990@gmail.com>
2018-12-31 22:02:41 -0500

    transitioning to react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Map, TileLayer} from 'react-leaflet'
+
+const accessToken = 'pk.eyJ1IjoiYW5haGFycmlzIiwiYSI6ImNqcWQyamVxOTBrMG40Mm4yYWFwYWtnc3gifQ.y6JLzfgsdsmZJqy1V1rsfg'
+const tileUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Map center={[38.9072, -77.0369]} zoom={12}>
+              <TileLayer
+                id={'mapbox.light'}
+                attribution={attribution}
+                url={tileUrl}
+                accessToken={accessToken}
+                />
+          </ Map>
       </div>
     );
   }
