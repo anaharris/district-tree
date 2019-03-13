c3e0320d486f471ca9889ac22a9eaddaa6ae814f
Ana Harris <vana1990@gmail.com>
2019-03-13 16:30:19 -0400

    deleted unused logos and pics, changed loading screen and added logo to about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-import {Sidebar, Segment, Divider, Header} from 'semantic-ui-react'
+import {Sidebar, Segment, Divider, Header, Image} from 'semantic-ui-react'
+import logo from './dt-logo.png'
 
 class About extends Component {
 
@@ -18,14 +19,7 @@ class About extends Component {
               paddingRight: '9%'
             }}
           >
-              <Header
-                as='h1'
-                size='huge'
-                style={{color: '#3a5344'}}
-              >
-                District Tree
-              </Header>
-            <Divider />
+            <Image src={logo} fluid/>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
