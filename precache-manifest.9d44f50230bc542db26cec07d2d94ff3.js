db1c40db4740afdf75058dd37680bb11040438d6
Ana Harris <vana1990@gmail.com>
2019-03-20 13:50:40 -0400

    Updates

---
+++ precache-manifest.9d44f50230bc542db26cec07d2d94ff3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/district-tree/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "a46ccce1cf99c8983192",
+    "url": "/district-tree/static/css/main.dffffb88.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/district-tree/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "5ab5081c2140b1adfbee",
+    "url": "/district-tree/static/js/1.5ab5081c.chunk.js"
+  },
+  {
+    "revision": "a321a8e5b60a11c7115a",
+    "url": "/district-tree/static/js/runtime~main.a321a8e5.js"
+  },
+  {
+    "revision": "a67f90e40d0c6d5ba9eadfb25781ca90",
+    "url": "/district-tree/static/media/dt-logo.a67f90e4.png"
+  },
+  {
+    "revision": "b7374a055833d88e63f8bcb957e88409",
+    "url": "/district-tree/static/media/about-icon.b7374a05.png"
+  },
+  {
+    "revision": "58e6c6c451f67ec3d2d8a83222eeaf60",
+    "url": "/district-tree/static/media/filter-icon.58e6c6c4.png"
+  },
+  {
+    "revision": "84feceed494ac592f53d194808559962",
+    "url": "/district-tree/static/media/loading-screen.84feceed.png"
+  },
+  {
+    "revision": "abb149dc167d382c1c0bd3fb4a17f209",
+    "url": "/district-tree/static/media/marker.abb149dc.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/district-tree/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/district-tree/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/district-tree/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "a46ccce1cf99c8983192",
+    "url": "/district-tree/static/js/main.a46ccce1.chunk.js"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/district-tree/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/district-tree/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/district-tree/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/district-tree/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/district-tree/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/district-tree/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/district-tree/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/district-tree/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/district-tree/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/district-tree/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/district-tree/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "5ab5081c2140b1adfbee",
+    "url": "/district-tree/static/css/1.a7e0eed7.chunk.css"
+  },
+  {
+    "revision": "50ddc52a9a8859d2f2651334aa641d1f",
+    "url": "/district-tree/index.html"
+  }
+];
